96c304e4ed5edddae61c6474f1efb49a0dfe0a2f
devYaksha <gssantoz2012@gmail.com>
2023-11-26 15:12:10 -0300

    generic headers

--- src/window_functions/window_book_functions.h
+++ src/window_functions/window_book_functions.h
@@ -8,12 +8,5 @@ LRESULT CALLBACK Window_Search_Book(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM 
 LRESULT CALLBACK Windwow_Loan_book(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 LRESULT CALLBACK Window_return_book(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-int print_Book();
-int insert_Book();
-int search_Book();
-int loan_book();
-int return_book();
-
-
 
 #endif // WINDOW_BOOK_FUNCTIONS_H
--- src/window_functions/window_database_functions.h
+++ src/window_functions/window_database_functions.h
@@ -5,8 +5,5 @@
 LRESULT CALLBACK Window_Unsorted_DataBase_Search(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 LRESULT CALLBACK Window_Sorted_DataBase_Search(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-int Window_Unsorted_DataBase();
-int Window_Sorted_DataBase();
-
 
 #endif // WINDOW_DATABASE_FUNCTIONS_H
